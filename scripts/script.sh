401ebe73317a9a553483151aecd8367d79373be5
AnujaKanade0410 <anuj.itworld@gmail.com>
2024-10-29 12:15:59 +0100

    Initial commit for Spring cloud config server

---
+++ scripts/script.sh
@@ -0,0 +1,24 @@
+echo "Assuming that docker-compose and vault CLI are installed"
+echo "If not check https://docs.docker.com/compose/install/ for Docker compose installation"
+echo "and https://www.vaultproject.io/docs/install for vault CLI"
+
+#Run vault
+#docker run \
+# -p 8200:8200 \
+# -e 'VAULT_DEV_ROOT_TOKEN_ID=vault-plaintext-root-token' \
+# -e 'VAULT_ADDR=http://0.0.0.0:8200' \
+# vault:1.13.3
+
+echo "Waiting for vault to start..."
+
+echo "Add entries to vault"
+export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_TOKEN='hvs.wQstneKgCWNo0dMxGrB8qvuV'
+
+echo "Adding a property for all applications (same as application.yml)"
+vault kv put secret/application only.in.vault="Hello"
+echo "Adding a property for app bar with profile foo"
+vault kv put secret/bar,foo a="bar with foo profile value from vault"
+echo "Reading the properties back"
+vault kv get secret/application
+vault kv get secret/bar,foo
